171a77888a840664ec1f7e4c9642f6fc9e944354
Tuanh4198 <nguyenanh.it.4198@gmail.com>
2021-08-22 18:29:07 +0700

    update responsive

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,18 @@ jQuery(document).ready(function($) {
 	$("#modal").load("modal.html");
 
 	// call slick slide
+	if($(window).width() <= 575) {
+		$('.two-rows').each(function() {
+			$(this).attr('data-rows', 2);
+		})
+		$('.has-dots').each(function() {
+			$(this).attr('data-dots', true);
+		})
+		$('.close-center-mode').each(function() {
+			$(this).attr('data-center-mode', false);
+			$(this).attr('data-center-padding', 0);
+		})
+	}
     $(".autoplay").each(function() {
         $(this).slick($(this).data());
     });
---
+++ readme.txt
@@ -0,0 +1,16 @@
+hướng dẫn sử dụng modal
+- nút mở modal PHẢI CÓ class btn-modal và data-modal="/id của modal/"
+demo
+<button class="btn-modal" data-modal="form-registration">ĐĂNG KÍ KHÁM</button>
+
+- nút close: PHẢI CÓ class btn-modal
+demo
+<button class="btn-close">
+	<img src="./images/home/close.png" alt=""/>
+</button>
+
+- Mmodal: PHẢI CÓ class modal-custom và 1 d riêng
+demo
+<div class="modal-custom" id="...">
+	/ nội dung
+</div>
